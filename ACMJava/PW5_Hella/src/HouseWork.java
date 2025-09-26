aed66caf262ed6944420610dc6b77c2ca1a1b900
YizhakVTumani <gns2008sng@gmail.com>
2025-09-26 17:38:19 +0300

    PW_5_settingColor

--- ACMJava/PW5_Hella/src/HouseWork.java
+++ ACMJava/PW5_Hella/src/HouseWork.java
@@ -1,7 +1,4 @@
-import acm.graphics.GLabel;
-import acm.graphics.GLine;
-import acm.graphics.GRect;
-import acm.graphics.GRectangle;
+import acm.graphics.*;
 import acm.program.*;
 
 import java.awt.*;
@@ -12,29 +9,40 @@ public class HouseWork extends GraphicsProgram {
     private static final double height = 500;
     private static final double width = 500;
     private static final double heightOfTheRoof = 100;
-    private static final double widthOfTheWindow = 100;
-    private static final double heightOfTheWindow = 100;
+    private static final double widthOfTheWindow = 75;
+    private static final double heightOfTheWindow = 75;
+    private static final int quantityOfSmokes = 2;
+    private static final double heightOfTheCloud = 20;
+    private static final double widthOfTheCloud = 40;
+    private static final int startPositionFromHouse = 100;
+    private static final int endPositionFromHouse = 100;
+    private static final boolean isDay = true;
 
     public void run(){
         this.setSize((int) width, (int) height);
-
+        backgroundCreator();
         pipeCreator();
         roofCreator();
         houseCreator();
         windowCreator();
         fenceCreator();
-
+        smokeCreator();
     }
     private void roofCreator(){
-        for (int i = 0; i < width/3; i += 3){
+        for (double i = 0; i < width/3; i += 1){
             GLine line = new GLine(width/2, height/3 - heightOfTheRoof,2 * width/3 - i , height/3);
-            line.setColor(Color.RED);
+            line.setColor(new Color(133, 100, 30));
+            add(line);
+        }
+        for (double i = 0; i < width/3; i += 4){
+            GLine line = new GLine(width/2, height/3 - heightOfTheRoof,2 * width/3 - i , height/3);
+            line.setColor(new Color(79, 58, 21));
             add(line);
         }
     }
     private void houseCreator(){
         GRect rect = new GRect(width/3, height/3, width/3, height/3);
-        rect.setFillColor(Color.GRAY);
+        rect.setColor(new Color(150, 143, 132));
         rect.setFilled(true);
         add(rect);
     }
@@ -43,7 +51,7 @@ public class HouseWork extends GraphicsProgram {
         add(line3);
         GLine line4 = new GLine(width/2 + width/12, height/3 - 2 * heightOfTheRoof/3, width/2 + width/12, height/3);
         add(line4);
-        for (int i = 0; i < height/3 - 2 * heightOfTheRoof; i += 1){
+        for (int i = 0; i < height/3; i += 1){
             GLine line5 = new GLine(width/2 + width/8, height/3 - 2 * heightOfTheRoof/3 + i, width/2 + width/12, height/3 - 2 * heightOfTheRoof/3 + i);
             line5.setColor(Color.GRAY);
             add(line5);
@@ -53,7 +61,7 @@ public class HouseWork extends GraphicsProgram {
         double middleX = width/2;
         double middleY = height/2;
         GRect rect = new GRect(middleX - widthOfTheWindow / 2, middleY - heightOfTheWindow / 2, widthOfTheWindow, heightOfTheWindow);
-        rect.setFillColor(Color.CYAN);
+        rect.setColor(new Color(217, 145, 41));
         rect.setFilled(true);
         add(rect);
         GLine line0 = new GLine (middleX, middleY - heightOfTheWindow / 2, middleX, middleY + heightOfTheWindow / 2);
@@ -62,10 +70,8 @@ public class HouseWork extends GraphicsProgram {
         add(line1);
     }
     private void fenceCreator(){
-        int startPositionFromHouse = 100;
-        int endPositionFromHouse = 100;
         for (double i = 0; i < startPositionFromHouse + endPositionFromHouse + width/3; i += 15 ){
-            GLine line5 = new GLine(width/3 - startPositionFromHouse + i - 7, 21 * height/36, width/3 - startPositionFromHouse + i + 15, 21 * height/36);
+            GLine line5 = new GLine(width/3 - startPositionFromHouse + i - 7, 42 * height/72, width/3 - startPositionFromHouse + i + 15, 21 * height/36);
             add(line5);
             GLine line6 = new GLine(width/3 - startPositionFromHouse + i - 7, 41 * height/72, width/3 - startPositionFromHouse + i + 15, 41 * height/72);
             add(line6);
@@ -83,6 +89,51 @@ public class HouseWork extends GraphicsProgram {
             add(line3);
             GLine line4 = new GLine(width/3 - startPositionFromHouse + i, 6 * height/9, width/3 - startPositionFromHouse + i + 6, 6 * height/9);
             add(line4);
+            for (double j = 1; 41 * height/72 + j < 42 * height/72 ; j++){
+                GLine filler = new GLine(width/3 - startPositionFromHouse + i - 7, 41 * height/72 + j, width/3 - startPositionFromHouse + i + 15, 41 * height/72 + j);
+                filler.setColor(new Color(115, 63, 53));
+                add(filler);
+            }
+            for (double j = 1; 45 * height/72 + j + 1 < 46 * height/72 ; j++){
+                GLine filler = new GLine(width/3 - startPositionFromHouse + i - 7, 45 * height/72 + j, width/3 - startPositionFromHouse + i + 15, 45 * height/72 + j);
+                filler.setColor(new Color(115, 63, 53));
+                add(filler);
+            }
+            for (int j = 1; j < 6; j++){
+                GLine filler = new GLine(width/3 - startPositionFromHouse + i + j, 6 * height/9 - 1,width/3 - startPositionFromHouse + i + j, 5 * height/9);
+                filler.setColor(new Color(115, 63, 53));
+                add(filler);
+            }
+            for (int j = 1; j < 6 ; j++){
+                GLine filler = new GLine(width/3 - startPositionFromHouse + i + 3, 5 * height/9 - 7 + 2,width/3 - startPositionFromHouse + i + j, 5 * height/9);
+                filler.setColor(new Color(115, 63, 53));
+                add(filler);
+            }
         }
+    }
+    private void smokeCreator(){
+        int distance = 0;
+        for (int i = quantityOfSmokes; i > 0; i--){
+            for(int j = 0; j < 30; j+=10){
+                GOval oval = new GOval(width/2 + width/12 - widthOfTheCloud/5 + j + distance, height/3 - 2 * heightOfTheRoof/3 - 30 - j - distance, widthOfTheCloud, heightOfTheCloud);
+                oval.setFilled(true);
+                oval.setColor(new Color(151, 170, 184));
+                add(oval);
+            }
+            distance += 50;
+        }
+    }
+    private void backgroundCreator(){
+        GRect rectSky = new GRect(0, 0, width, 5 * height/9);
+        rectSky.setFilled(true);
+        rectSky.setColor(new Color(43, 169, 181));
+        add(rectSky);
+        GRect rectGround = new GRect(0, 5 * height/9, width, height);
+        rectGround.setFilled(true);
+        rectGround.setColor(new Color(33, 133, 30));
+        add(rectGround);
+    }
+    private void setCustomColor(){
+
     }
 }
