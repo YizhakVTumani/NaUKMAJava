fb25ce45ebc9f923a5d879c0548e36939d83607e
YizhakVTumani <gns2008sng@gmail.com>
2025-09-30 18:36:50 +0300

    HW5_done

--- ACMJava/HW5_Hella/src/SunWork.java
+++ ACMJava/HW5_Hella/src/SunWork.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class SunWork extends GraphicsProgram {
 
-    private static final double sunraysQuantity = 1000;
+    private static final double sunraysQuantity = 50;
 
     public void run(){
         this.setSize(800,800);
@@ -23,9 +23,10 @@ public class SunWork extends GraphicsProgram {
     private void sunraysQuantityCalculator(){
         for (double i = 1; i <= sunraysQuantity; i ++){
             double rays = Math.toRadians(360) / sunraysQuantity * i;
-            double xPoint = 400 + 150 * Math.cos(rays);
-            double yPoint = 400 + 150 * Math.sin(rays);
+            double xPoint = 400 + 400 * Math.cos(rays);
+            double yPoint = 400 + 400 * Math.sin(rays);
             GLine line = new GLine(400,400,xPoint,yPoint);
+            line.setColor(Color.YELLOW);
             add(line);
         }
     }
