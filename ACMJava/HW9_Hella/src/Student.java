8035a23e89ee39c8d9c0c72985a5669e4999ee95
YizhakVTumani <gns2008sng@gmail.com>
2025-11-04 19:30:18 +0200

    hw9

--- ACMJava/HW9_Hella/src/Student.java
+++ ACMJava/HW9_Hella/src/Student.java
@@ -1,48 +1,36 @@
 import acm.program.*;
 
 //Author: Hella Nikita
-//File: HelloProgram.java
+//File: Student.java
 
+//Написати клас, який описує студента НаУКМА використовуючи наступні елементи:
+//
+//змінні екземпляра
+//змінні класу
+//public і private методи
+//javadoc
+//toString
+//Та клас який використовує клас Student.
 
 public class Student extends ConsoleProgram {
- public static int numberOfStudents;
- private String fio;
- private int mark;
- private int course;
+
 
     public void run(){
         println("Welcome to the Student Program");
 
-        StudentsConstructor student = new StudentsConstructor(fio);
-        student.setFio("Hella Nikita");
-        println("Student fio is: " + student.getFio() + ". Student mark is: " + student.getMark() + ". Student course is: " + student.getCourse());
+        StudentsConstructor student = new StudentsConstructor("Nikita Serhiyovych" );
+//        student.setFio("Hella Nikita");
+        println("Student fio is: " + student.fio + ". Student mark is: " + student.mark + ". Student course is: " + student.course);
 
-        StudentsConstructor student2 = new StudentsConstructor(fio, mark, course);
-        student2.setFio("Sophia Karachentseva");
-        student2.setMark(90);
-        student2.setCourse(1);
-        println("Student fio is: " + student2.getFio() + ". Student mark is: " + student2.getMark() + ". Student course is: " + student2.getCourse());
+        StudentsConstructor student2 = new StudentsConstructor("Khurschev", 90, 1);
+//        student2.setFio("Oleh Lyashko");
+//        student2.setMark(90);
+//        student2.setCourse(1);
+        println("Student fio is: " + student2.fio + ". Student mark is: " + student2.mark + ". Student course is: " + student2.course);
 
         StudentsConstructor student3 = new StudentsConstructor();
-        println("Student fio is: " + student3.getFio() + ". Student mark is: " + student3.getMark() + ". Student course is: " + student3.getCourse());
+        println("Student fio is: " + student3.fio + ". Student mark is: " + student3.mark + ". Student course is: " + student3.course);
     }
 
-    public int getMark(){
-        return mark;
-    }
-    public int getCourse(){
-        return course;
-    }
-    public void setMark(int mark){
-        this.mark = mark;
-    }
-    public void setCourse(int course){
-        this.course = course;
-    }
-    public String getFio(){
-        return fio;
-    }
-    public void setFio(String fio){
-        this.fio = fio;
-    }
+
 }
--- ACMJava/HW9_Hella/src/StudentsConstructor.java
+++ ACMJava/HW9_Hella/src/StudentsConstructor.java
@@ -1,18 +1,62 @@
 import acm.program.ConsoleProgram;
 
 //Author: Hella Nikita
-//File: HelloProgram.java
+//File: StudentsConstructor.java
 
+//У Вас мають бути різні конструктори класів (обов'язково: порожній; частковий - тільки ПІБ; та  повний - з усіма параметрами)
 
 public class StudentsConstructor extends Student {
+    public static int numberOfStudents;
 
+    public String fio;
+    public int mark;
+    public int course;
+
+    /**
+     * empty constructor
+     */
     StudentsConstructor(){
         numberOfStudents++;
     }
+
+    /**
+     * one only parameter constructor
+     * @param fio - Name and Surname strings
+     */
     StudentsConstructor(String fio){
         numberOfStudents++;
+        this.fio = fio;
     }
+
+    /**
+     * A full parameter constructor
+     * @param fio - Name and Surname strings
+     * @param mark - int of the mark
+     * @param course - number of course
+     */
     StudentsConstructor(String fio, int mark, int course){
         numberOfStudents++;
+        this.fio = fio;
+        this.course = course;
+        this.mark = mark;
     }
+
+//    public int getMark(){
+//        return mark;
+//    }
+//    public int getCourse(){
+//        return course;
+//    }
+//    public void setMark(int mark){
+//        this.mark = mark;
+//    }
+//    public void setCourse(int course){
+//        this.course = course;
+//    }
+//    public String getFio(){
+//        return fio;
+//    }
+//    public void setFio(String fio){
+//        this.fio = fio;
+//    }
 }
