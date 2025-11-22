2173ecdcfea80b66e0f441b9c03da23d49d9efb5
YizhakVTumani <gns2008sng@gmail.com>
2025-11-22 10:07:37 +0200

    commit

--- ACMJava/PW13_Hella/src/HelloProgram.java
+++ ACMJava/PW13_Hella/src/HelloProgram.java
@@ -1,9 +1,10 @@
 import acm.graphics.*;
 import acm.program.*;
+import acm.util.ErrorException;
 
 import java.awt.*;
-import java.util.Random;
-import java.util.random.*;
+import java.io.*;
+import java.util.*;
 
 //Author: Hella Nikita
 //File: HelloProgram.java
@@ -15,7 +16,29 @@ public class HelloProgram extends ConsoleProgram {
             int number = readInt("Please enter a number: ");
             intToString(number);
         }while(readInt("to repeat press 1: ") == 1);
+
+        do{
+            String str = readLine("Please enter a string: ");
+            String strCh = readLine("Please enter a char: ");
+            char ch = strCh.charAt(0);
+            println("Res string is: " + removeAllOccurences(str, ch));
+        }while(readInt("to repeat press 1: ") == 1);
+
+        BufferedReader myR = fileToString("Print your name of the file: ");
+        try {
+            while (true){
+                String s;
+                s = myR.readLine();
+                if (s==null) break;
+                println(s);
+            }
+            myR.close();
+        }catch (IOException e) {
+            throw new ErrorException(e);
+        }
     }
+
+
     private void intToString(int number){
         String res = "";
         String num = " " + number;
@@ -26,5 +49,34 @@ public class HelloProgram extends ConsoleProgram {
                 res = res + num.charAt(i);
             }
         println(res);
+    }
+
+
+    public String removeAllOccurences(String str, char ch){
+        String res = "";
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) != ch){
+                res = res + str.charAt(i);
+            }
+        }
+        return res;
+    }
+
+
+    private BufferedReader fileToString(String prompt){
+        BufferedReader rd = null;
+        while (rd == null){
+            try{
+                String name = readLine(prompt);
+                rd = new BufferedReader( new FileReader(name));
+            } catch (FileNotFoundException e){
+                println("Exeption occurred while trying to open the file.");
+                //e.printStackTrace();
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return rd;
     }
 }
